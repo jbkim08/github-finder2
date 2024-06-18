7d6e68f079364935d071e6603837b5be19753b04
JBKIM <drv98@naver.com>
2024-06-18 11:13:34 +0900

    로딩중 스피너 표시

--- src/components/users/UserResult.jsx
+++ src/components/users/UserResult.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
 
 function UserResult() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetchUsers();
   }, []);
@@ -13,14 +15,21 @@ function UserResult() {
     });
     const data = await response.json(); //제이슨 변환
     setUsers(data); //유저들을 저장
+    setLoading(false); //데이터 로딩 완료
   };
-  return (
-    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-      {users.map((user) => (
-        <h3>{user.login}</h3>
-      ))}
-    </div>
-  );
+
+  //로딩중일때 ... 완료시 유저표시
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3 key={user.id}>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 }
 
 export default UserResult;
