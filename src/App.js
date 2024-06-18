ddfcc9469fb12065eb39da0b9cd2a5918da51a7b
JBKIM <drv98@naver.com>
2024-06-18 15:30:48 +0900

    경고메세지 업데이트

--- src/App.js
+++ src/App.js
@@ -6,25 +6,28 @@ import Home from "./components/pages/Home";
 import NotFound from "./components/pages/NotFound";
 import About from "./components/pages/About";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 
 function App() {
   return (
-    <GithubProvider>
-      <BrowserRouter>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/notfound" element={<NotFound />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </BrowserRouter>
-    </GithubProvider>
+    <AlertProvider>
+      <GithubProvider>
+        <BrowserRouter>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </GithubProvider>
+    </AlertProvider>
   );
 }
 
--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,15 +1,17 @@
 import React, { useContext, useState } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
 
 function UserSearch() {
   const [text, setText] = useState("");
 
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
 
   const handleSubmit = (e) => {
     e.preventDefault(); //기존 이벤트 중지
     if (text === "") {
-      alert("내용을 입력해주세요!");
+      setAlert("내용을 입력해주세요!", "error");
     } else {
       //유저찾기
       searchUsers(text);
