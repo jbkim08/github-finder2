262dff2221b917e9c5b6b90180fb5e42c42a0fd8
JBKIM <drv98@naver.com>
2024-06-18 14:08:04 +0900

    리듀서 추가

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,12 +1,20 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
+import githubReducer from "./GithubReducer";
+import { type } from "@testing-library/user-event/dist/type";
 
 const GithubContext = createContext();
 
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  //const [users, setUsers] = useState([]);
+  //const [loading, setLoading] = useState(true);
+  //스테이트의 초기값
+  const initialState = {
+    users: [],
+    loading: true,
+  };
+  const [state, dispatch] = useReducer(githubReducer, initialState);
   const fetchUsers = async () => {
     const response = await fetch("https://api.github.com/users", {
       headers: {
@@ -14,11 +22,17 @@ export const GithubProvider = ({ children }) => {
       },
     });
     const data = await response.json(); //제이슨 변환
-    setUsers(data); //유저들을 저장
-    setLoading(false); //데이터 로딩 완료
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
+    //setUsers(data); //유저들을 저장
+    //setLoading(false); //데이터 로딩 완료
   };
   return (
-    <GithubContext.Provider value={{ users, loading, fetchUsers }}>
+    <GithubContext.Provider
+      value={{ users: state.users, loading: state.loading, fetchUsers }}
+    >
       {children}
     </GithubContext.Provider>
   );
