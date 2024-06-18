3760611d21e6121effc2ac43f6e088bd9b574d44
JBKIM <drv98@naver.com>
2024-06-18 09:12:15 +0900

    푸터추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Navbar from "./components/layout/Navbar";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-
         <main>Centent</main>
+        <Footer />
       </div>
     </BrowserRouter>
   );
