734ab06c67a1884e44a74cf9c46b2f60c4cbae43
JBKIM <drv98@naver.com>
2024-06-18 11:40:50 +0900

    깃허브 컨텍스트

--- src/App.js
+++ src/App.js
@@ -5,23 +5,26 @@ import Footer from "./components/layout/Footer";
 import Home from "./components/pages/Home";
 import NotFound from "./components/pages/NotFound";
 import About from "./components/pages/About";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <GithubProvider>
+      <BrowserRouter>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </GithubProvider>
   );
 }
 
--- src/components/users/UserResult.jsx
+++ src/components/users/UserResult.jsx
@@ -1,23 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
 
 function UserResult() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
+
   useEffect(() => {
     fetchUsers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  const fetchUsers = async () => {
-    const response = await fetch("https://api.github.com/users", {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-    const data = await response.json(); //제이슨 변환
-    setUsers(data); //유저들을 저장
-    setLoading(false); //데이터 로딩 완료
-  };
 
   //로딩중일때 ... 완료시 유저표시
   if (!loading) {
