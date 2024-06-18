3f30329c516cfb46c3627fb7e03ce625eb71a318
JBKIM <drv98@naver.com>
2024-06-18 15:13:59 +0900

    유저 클리어

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -52,10 +52,20 @@ export const GithubProvider = ({ children }) => {
     dispatch({
       type: "SET_LOADING",
     });
+  //유저 클리어
+  const clearUsers = () =>
+    dispatch({
+      type: "CLEAR_USERS",
+    });
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, searchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
